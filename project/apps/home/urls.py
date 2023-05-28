08bc15d0071b0424069a2d773542650a3ae72493
Pablo Pineda H <pablopinedah@gmail.com>
2023-05-28 18:58:32 -0500

    Crearon formularios: Cliente,  Alcance 1 y 2.

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('crear-cliente/', views.crear_cliente, name="crear-cliente"),
+    path('crear-alcance1/', views.crear_alcance1, name="crear-alcance1"),
+    path('crear-alcance2/', views.crear_alcance2, name="crear-alcance2"),
 ]
